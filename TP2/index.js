34b22be682e1843e0042db6b46996b52257f5ebf
Damian <demiantriebl@gmail.com>
2022-07-16 17:02:48 -0300

    TP2: Manejo de archivos

---
+++ TP2/index.js
@@ -0,0 +1,9 @@
+const Contenedor = require('./Contenedor');
+
+const negocio  = new Contenedor('./TP2/productos.txt');
+
+//negocio.save({title: 'alpargata azul', price: 5090, thumbnail: 'http....'});
+//negocio.getById(2);
+//negocio.getAll();
+//negocio.deleteById(1);
+negocio.deleteAll();
