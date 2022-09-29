927bb981ddd74d77aa31af744c4c234d7eb8b0ef
Damian <demiantriebl@gmail.com>
2022-09-29 19:35:31 -0300

    tp8 session redis

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import express  from "express";
 import session from "express-session";
-import redis from 'redis'
+import {createClient} from 'redis'
 import connectRedis from "connect-redis";
 
 const app = express()
@@ -9,9 +9,13 @@ app.use(express.urlencoded({extended:true}))
 
 const redisStore = connectRedis(session)
 
-const redisClient = redis.createClient({
-    host: 'redis-12757.c14.us-east-1-2.ec2.cloud.redislabs.com:12757',
-    port: 12757
+const redisClient = createClient({
+    socket:{
+        host: 'redis-12757.c14.us-east-1-2.ec2.cloud.redislabs.com',
+        port: 12757,
+    },
+    password: 'lWsQsGAK4er3PPFSp5MZoSBS3V5tsSgG',
+    legacyMode:true     
 })
 redisClient.on('error', function (err) {
     console.log('no se puede establecer conexion con redis. ' + err);
@@ -19,6 +23,7 @@ redisClient.on('error', function (err) {
 redisClient.on('connect', function (ok) {
     console.log('se conecto redis');
 });
+redisClient.connect();
 //Configuramos el middleware
 app.use(session({
     store: new redisStore({ client: redisClient }),
@@ -28,12 +33,11 @@ app.use(session({
     cookie: {
         secure: false, 
         httpOnly: false, 
-        maxAge: 10000 // session en milisegundos
     }
 }))
 app.get('/', (req, res) => {
     const sess = req.session;
-    if(sess.usuario && sess.password){
+    if(sess.username && sess.password){
             res.write(`<h1>Bievenido ${sess.username} </h1><br>`)
             res.end('<a href=' + '/logout' + '>Cerrar Sesion</a >')
         
