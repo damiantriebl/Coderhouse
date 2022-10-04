7093995d11984af79a2737eaef26c51cb5903985
damian triebl <damiantriebl@gmail.com>
2022-10-04 05:30:44 -0700

    chequeo de la after class

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 # Coderhouse
-## Normalizr  
+## Entrega Final
 
-revision de normalizr, se puede normalizar, pero al no guardar la entidad texto, no se puede denormalizar me manera normal, busque documentacion pero es muy escaso, me guie por esto , pero aun asi no funciona solo la parte de normalizar.
-
-[documentacion](https://github.com/paularmstrong/normalizr/blob/master/docs/api.md#objectdefinition)
-
+Este proyecto esta hecho en dos capas, con una capa de frontend (client, hecho en react con vite, y estilos en tailwind) y otra de backend (server). tiene un carro (se usa un solo id de cliente, con ID: 1 , pero si se utilizara otro se podria tranquilamente
 
 
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,87 +1,25 @@
-import { useState, useEffect } from 'react'
-import Mensajes from './mensajes';
-import { faker } from '@faker-js/faker';
-import axios from 'axios'
-import './App.css'
-import useRequest from './hooks/useRequest';
-import {denormalize , normalize, schema} from 'normalizr'
-
-
-const App = () => {
-  const [body, setBody] = useState();
-  const [data, setData] = useState();
-  const [porcentaje, setPorcentaje] = useState();
-
-  const {doSend , errors } = useRequest({
-    url: `/mensaje`,
-    method: "post",
-    body: body,
-    onSuccess: (objs) =>{ 
-      console.log(objs)
-      doGet()
-    }
-  });
-  
-  const {doSend:doGet , errors: getErrors } = useRequest({
-    url: `/mensaje`,
-    method: "get",
-    onSuccess: (obj) =>{
-      console.log('tirando el obj',)
-      const authorSchema = new schema.Entity('author', {}, {idAttribute: "id"});
-    const commentSchema = new schema.Entity('texto');        
-        const responseSchema = new schema.Array({ author: authorSchema, texto:commentSchema  });
-     
-      const denormalizado = denormalize(obj.result, responseSchema ,obj.entities)
-      const porciento = (JSON.stringify(denormalizado).length / JSON.stringify(obj).length * 100).toFixed(2)
-      console.log('normalizado ', denormalizado)
-      setData(denormalizado)
-      setPorcentaje(porciento)
-    }
-  });
-
-  useEffect (()=>{
-    doGet();
-  },[]) 
-  
-  const handleSubmit = async (e) =>{
-    e.preventDefault();
-    const data = {
-      author:{
-        id: e.target[0].value,
-        nombre:  e.target[1].value,
-        apellido: e.target[2].value,
-        edad: e.target[3].value,
-        alias:  e.target[4].value,
-        avatar:  e.target[5].value,
-      },
-      texto:   e.target[6].value,    
-    }   
-    setBody(data)
-    doSend();
-  } 
-  
+import Comments from "./Layout/Comments";
+import Products from "./Layout/Products";
+import Home from './Layout/Home'
+import Nav from "./Layout/Nav";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Cart from "./Layout/Cart";
+function App() {
+  const home =( useLocation().pathname === '/');
+  console.log('la locacion es ', home)
   return (
     <div className="App">
-      <h1>{porcentaje && `el factor de compresion es %${porcentaje}`}</h1>
-      <h1>Centro de mensajes</h1>
-      <form onSubmit={handleSubmit} className="formClass">
-        <input type="email" placeholder='email' name="id" />
-        <input type="text" placeholder='nombre'  name="nombre" />
-        <input type="text" placeholder='apellido' name="apellido"/>
-        <input type="text" placeholder='edad' name="edad"/>
-        <input type="text" placeholder='alias' name="alias" />
-        <input type="text" placeholder='avatar' name="avatar" />
-        <input type="text" placeholder='texto' name="text" />
-        <button type='submit' >Enviar Mensjae</button>
-      </form>
-      <div className='footer'>        
-        {console.log('la data es ', data)}
-        {data && data.map((obj)=>{
-          return (<Mensajes obj={obj} />)
-        })}
+      <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50">
+        <Nav home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/productos" element={<Products />} />
+          <Route path="carrito/:userId" element={<Cart />} />
+        </Routes>
       </div>
+      <Comments home className={home && "hidden "} />
     </div>
-  )
+  );
 }
 
 export default App;
