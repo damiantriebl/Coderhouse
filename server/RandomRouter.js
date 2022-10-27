29aca71914e0c02bf24502e5aeb1261ea19eb3af
damian triebl <damiantriebl@gmail.com>
2022-10-27 19:10:35 -0300

    artillery y logger

--- server/RandomRouter.js
+++ server/RandomRouter.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import compression from 'compression'
 const router = express.Router();
 import {fork} from 'child_process';
 
 
-router.get("/test/random/:cant", (req, res) => {
+router.get("/test/random/:cant", compression(), (req, res) => {
+  
   const cantidad = parseInt(req.params.cant) || 1e6;
   const forkeado = fork("./configuracion/forkeado.js");
   forkeado.send(cantidad);
